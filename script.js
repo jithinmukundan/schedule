7d63478367cf73896f85f391cc55434fea250916
jithinmukundan <jithinmukundan@gmail.com>
2023-05-13 16:27:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-window.onload = function() {
-    var date = new Date();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-    var firstDay = new Date(year, month, 1).getDay();
+var date = new Date();
+var currentMonth = date.getMonth();
+var currentYear = date.getFullYear();
 
+window.onload = function() {
+    drawCalendar(currentMonth, currentYear);
+}
+
+function drawCalendar(month, year) {
+    var firstDay = new Date(year, month, 1).getDay();
     var daysInMonth = 32 - new Date(year, month, 32).getDate();
 
     var calendar = document.getElementById('calendar');
+    calendar.innerHTML = '';
+
+    // Add month navigation
+    var prevButton = document.createElement('button');
+    prevButton.innerText = '<';
+    prevButton.onclick = function() {
+        if (month > 0) {
+            drawCalendar(month - 1, year);
+        } else {
+            drawCalendar(11, year - 1);
+        }
+    };
+    var nextButton = document.createElement('button');
+    nextButton.innerText = '>';
+    nextButton.onclick = function() {
+        if (month < 11) {
+            drawCalendar(month + 1, year);
+        } else {
+            drawCalendar(0, year + 1);
+        }
+    };
+    var monthLabel = document.createElement('div');
+    monthLabel.innerText = new Date(year, month).toLocaleString('default', { month: 'long' }) + ' ' + year;
+    monthLabel.colSpan = 5;
+    var navRow = document.createElement('div');
+    navRow.appendChild(prevButton);
+    navRow.appendChild(monthLabel);
+    navRow.appendChild(nextButton);
+    calendar.appendChild(navRow);
 
     // Add blank days
     for (var i = 0; i < firstDay; i++) {
@@ -20,6 +53,9 @@ window.onload = function() {
         var cell = document.createElement('div');
         cell.classList.add('date');
         cell.innerText = i;
+        cell.onclick = function() {
+            alert('Selected date: ' + this.innerText + ' ' + new Date(year, month).toLocaleString('default', { month: 'long' }) + ', ' + year);
+        };
         calendar.appendChild(cell);
     }
 }
